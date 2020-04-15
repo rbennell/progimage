00d8f24af6421f7925f6cd33ff43166a36f1457e
Rob <rbennell@hotmail.co.uk>
2020-04-15 13:08:21 +0100

    Add basic upload/download

--- src/progimage.py
+++ src/progimage.py
@@ -1,8 +1,31 @@
-from flask import Flask
+import os
+import uuid
+
+from flask import Flask, request, send_from_directory, redirect
 
 app = Flask(__name__)
+app.config["IMAGES"] = "/images"
 
 
 @app.route("/")
 def hello_world():
     return "Hello, Heycar!"
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    if "file" not in request.files:
+        print("No file part")
+        return redirect(request.url)
+    file = request.files["file"]
+    if file.filename == "":
+        print("No selected file")
+        return redirect(request.url)
+    image_id = str(uuid.uuid4())
+    file.save(os.path.join(app.config["IMAGES"], image_id + ".jpeg"))
+    return image_id
+
+
+@app.route("/image/<string:image_id>", methods=["GET"])
+def get_image(image_id):
+    return send_from_directory(app.config["IMAGES"], image_id + ".jpeg")
