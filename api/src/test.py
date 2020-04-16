3d23604454c02beef11af10d01bde4aa85b69d39
Rob <rbennell@hotmail.co.uk>
2020-04-16 19:36:52 +0100

    Fix api tests!
    After making some last minute changes to endpoints i forgot to fix the tests. oops!

--- api/src/test.py
+++ api/src/test.py
@@ -4,7 +4,7 @@ from unittest import mock, TestCase
 from api import BadRequest, ProgImageApi
 
 
-PATH = "/get_image"
+PATH = "/image"
 QUERY_ARGS = {"rotate": 90, "filter": "blur"}
 
 
@@ -16,11 +16,11 @@ class TestApi(TestCase):
 class TestBuildUrl(TestApi):
     def test_url_with_path(self):
         url = self.api._build_url("/a/test/path")
-        self.assertEqual(url, "localhost:5000/a/test/path")
+        self.assertEqual(url, "http://web:5000/a/test/path")
 
     def test_url_with_path_and_query_args(self):
         url = self.api._build_url("/another/test/path", QUERY_ARGS)
-        self.assertEqual(url, "localhost:5000/another/test/path?rotate=90&filter=blur")
+        self.assertEqual(url, "http://web:5000/another/test/path?rotate=90&filter=blur")
 
     def test_url_with_new_endpoint(self):
         self.api.endpoint = "www.spacejam.com"
@@ -32,7 +32,7 @@ class TestBuildUrl(TestApi):
         url = self.api._build_url(PATH, {"url": url})
         self.assertEqual(
             url,
-            "localhost:5000/get_image?url=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F3543879283%2F1509e34005183da5ea4eb29150f341e5_400x400.jpeg",
+            "http://web:5000/image?url=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F3543879283%2F1509e34005183da5ea4eb29150f341e5_400x400.jpeg",
             "Assert the url is properly encoded",
         )
 
@@ -41,7 +41,7 @@ class TestGet(TestApi):
     @mock.patch("requests.get")
     def test_get(self, get_mock):
         self.api._get(PATH, QUERY_ARGS)
-        get_mock.assert_called_with("localhost:5000/get_image?rotate=90&filter=blur")
+        get_mock.assert_called_with("http://web:5000/image?rotate=90&filter=blur")
 
 
 class TestPost(TestApi):
@@ -49,7 +49,7 @@ class TestPost(TestApi):
     def test_post_without_files(self, post_mock):
         self.api._post(PATH, QUERY_ARGS)
         post_mock.assert_called_with(
-            "localhost:5000/get_image?rotate=90&filter=blur", files=None
+            "http://web:5000/image?rotate=90&filter=blur", files=None
         )
 
     @mock.patch("requests.post")
@@ -57,7 +57,7 @@ class TestPost(TestApi):
         files = {"file": "buffer"}
         self.api._post(PATH, QUERY_ARGS, files=files)
         post_mock.assert_called_with(
-            "localhost:5000/get_image?rotate=90&filter=blur", files={"file": "buffer"}
+            "http://web:5000/image?rotate=90&filter=blur", files={"file": "buffer"}
         )
 
 
@@ -102,18 +102,18 @@ class TestGetImage(TestApi):
     @mock.patch("api.ProgImageApi._get")
     def test_get_image_with_id_and_no_effects(self, get_image_mock):
         self.api.get_image(image_id=1234)
-        get_image_mock.assert_called_with("/get_image", {"image_id": 1234})
+        get_image_mock.assert_called_with("/image", {"image_id": 1234})
 
     @mock.patch("api.ProgImageApi._get")
     def test_get_image_with_url_and_no_effects(self, get_image_mock):
         self.api.get_image(url="http://images.com/image")
         get_image_mock.assert_called_with(
-            "/get_image", {"url": "http://images.com/image"}
+            "/image", {"url": "http://images.com/image"}
         )
 
     @mock.patch("api.ProgImageApi._get")
     def test_get_image_with_id_and_effects(self, get_image_mock):
         self.api.get_image(image_id=1234, **QUERY_ARGS)
         get_image_mock.assert_called_with(
-            "/get_image", {"image_id": 1234, "rotate": 90, "filter": "blur"}
+            "/image", {"image_id": 1234, "rotate": 90, "filter": "blur"}
         )
